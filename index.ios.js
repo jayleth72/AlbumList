ad9403691b0c33ea6b806c194ba59c3e59f86084
Jay Letheby <jay.letheby@gmail.com>
2017-04-30 17:48:00 +1000

    add react-native files

---
+++ index.ios.js
@@ -0,0 +1,20 @@
+// Index.ios.js
+
+// Import a library to help create a Component
+import React from 'react';
+import { AppRegistry, View } from 'react-native';
+import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
+
+// Create a Component
+const App = () => (
+  <View>
+    <Header headerText={'Albums'}/>
+    <AlbumList />
+  </View>
+  
+);
+
+
+// Render it to the device
+AppRegistry.registerComponent('Albums', () => App);
