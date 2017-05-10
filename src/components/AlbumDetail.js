114b9622de6d264239be65186f00da5645b8ae8b
Jay Letheby <jay.letheby@gmail.com>
2017-05-10 23:25:25 +1000

    added button and styling

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
   const { title, artist, thumbnail_image, image } = album;
@@ -28,11 +29,16 @@ const AlbumDetail = ({ album }) => {
         </View>
 
       </CardSection>
+
       <CardSection>
         <Image
            style={imageStyle}
            source={{ uri: image }}
         />
+      </CardSection>
+
+      <CardSection>
+        <Button onPress={() => console.log(title)}/>
       </CardSection>
     </Card>
   )
